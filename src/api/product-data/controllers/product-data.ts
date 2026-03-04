675650915a31beede63197fbf7e4b1999fbfdf08
mayank365pod <mayank@pods365.com>
2026-03-04 17:28:17 +0530

    first commit

---
+++ src/api/product-data/controllers/product-data.ts
@@ -0,0 +1,7 @@
+/**
+ * product-data controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::product-data.product-data');
---
+++ src/api/product-data/services/product-data.ts
@@ -0,0 +1,7 @@
+/**
+ * product-data service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::product-data.product-data');
